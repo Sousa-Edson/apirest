22ddad875e4e7d41f0183154ee3e76a8775bd284
Edson Sousa <edson3711.es@gmail.com>
2023-01-12 00:22:06 -0300

    @CrossOrigin

--- src/main/java/com/produtos/apirest/resources/ProdutoResource.java
+++ src/main/java/com/produtos/apirest/resources/ProdutoResource.java
@@ -4,6 +4,7 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -18,33 +19,34 @@ import com.produtos.apirest.repository.ProdutoRepository;
 
 @RestController
 @RequestMapping(value = "/api")
+@CrossOrigin
 public class ProdutoResource {
-    @Autowired
-    ProdutoRepository produtoRepository;
+	@Autowired
+	ProdutoRepository produtoRepository;
 
-    @GetMapping("/produto")
-    public List<Produto> listaProdutos() {
-        return produtoRepository.findAll();
-    }
+	@GetMapping("/produto")
+	public List<Produto> listaProdutos() {
+		return produtoRepository.findAll();
+	}
 
-    @GetMapping("/produto/{id}")
-    public Optional<Produto> listaProdutoUnico(@PathVariable(value = "id") Long id) {
-        return produtoRepository.findById(id);
-    }
+	@GetMapping("/produto/{id}")
+	public Optional<Produto> listaProdutoUnico(@PathVariable(value = "id") Long id) {
+		return produtoRepository.findById(id);
+	}
 
-    @PostMapping("/produto")
-    public Produto salvaProduto(@RequestBody Produto produto) {
-        return produtoRepository.save(produto);
-    }
+	@PostMapping("/produto")
+	public Produto salvaProduto(@RequestBody Produto produto) {
+		return produtoRepository.save(produto);
+	}
 
-    @DeleteMapping("/produto")
-    public void deletaProduto(@RequestBody Produto produto) {
-        produtoRepository.delete(produto);
-    }
+	@DeleteMapping("/produto")
+	public void deletaProduto(@RequestBody Produto produto) {
+		produtoRepository.delete(produto);
+	}
 
-    @PutMapping("/produto")
-    public Produto atualizaProduto(@RequestBody Produto produto) {
-        return produtoRepository.save(produto);
-    }
+	@PutMapping("/produto")
+	public Produto atualizaProduto(@RequestBody Produto produto) {
+		return produtoRepository.save(produto);
+	}
 
 }
